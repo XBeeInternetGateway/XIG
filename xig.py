7c30a6ea8d7a3ee3aa01b756696085e187c5eb4b
liminastudio <liminastudio@cbe2d5de-6f11-11de-b374-81e7cb8b6ca2>
2009-07-28 02:30:35 +0000

    Ted: Major change. Receives one byte at a time and checks for delimiter (CR)
    git-svn-id: https://xig.googlecode.com/svn/trunk@9 cbe2d5de-6f11-11de-b374-81e7cb8b6ca2

--- xig.py
+++ xig.py
@@ -1,14 +1,19 @@
 NAME = 'XBee Internet Gateway (xig)'
-VERSION = '1.00a26'
+VERSION = '1.00a27'
+TIMEOUT = 0                                    # default length of time (s) before main loop automatically times out, 0 runs forever
+SLEEP_DUR = 0.00                        # sleep delay
+TERMINATOR = "\r"                        # command terminator byte
+QUIT_CODE = "~"
 
 print NAME + ' v' + VERSION
 print 'unzipping and loading modules...'
+
 import sys, time
 from socket import *
 from select import *
 
 sys.path.append('WEB/python/url_libs.zip')
-sys.path.append('WEB/python/email.zip')
+#sys.path.append('WEB/python/email.zip')
 
 import urllib
 
@@ -29,8 +34,14 @@ write log header to file
 helpFile = '\n\r---------------------------\n\r'+ NAME + ' v' + VERSION + """
 \r  by Rob Faludi faludi.com\r
 \r
+ARGUMENTS:\r
+ timeout: Time in seconds before program automatically quits. If not used, program runs indefinitely.\r
 COMMANDS:\r
+ All commands are carriage-return (CR / ASCII 13 / "\\r") delimited, except where noted.\r
+\r
  help: displays this file\r
+ ~: quits program, does not require delimiting\r
+\r
  http://<host/path> receives a URL\r
  https://<host/path> receives a secure URL\r
  http://<host/path:port>\r
@@ -50,15 +61,16 @@ USE:\r
 ---------------------------\r
 """
 
-print 'initializing URL calls...'
-urllib.urlopen('http://www.google.com')
-
+#print 'initializing URL calls...'
+#urllib.urlopen('http://www.google.com')
 
 if len(sys.argv) > 1: # if there's an argument we use it for the stop time
     stopTime = int(sys.argv[1])
+    print 'Stopping after %d seconds' % (stopTime,)
 else:
-    stopTime = 0 # otherwise we keep running for 5 minutes
-print 'stopping after %d seconds' % (stopTime,)
+    stopTime = TIMEOUT # otherwise use default
+    print 'Running indefinitely. Send ~ to quit.'
+
 startTime = time.clock()
 
 ## create a socket using the ZigBee address family, datagram mode and a proprietary transport
@@ -67,45 +79,57 @@ sd = socket(AF_ZIGBEE, SOCK_DGRAM, ZBS_PROT_TRANSPORT)
 # end point, profile ID and cluster ID are zero when using the 802.15.4 radios
 sd.bind(("",0xe8,0,0))
 
-# Configure the socket for non-blocking operation: 
+# Configure the socket for non-blocking operation:
 sd.setblocking(0)
 
-# Initialize state variables: 
-response = "" 
+# Initialize state variables:
+data = ""                        # holds the incoming data
+buf = ""                        # holds collected data
+response = ""                # parsed buffer
 src_addr = ()
+request = ""
 
 while (time.clock()-startTime < stopTime or stopTime==0):
     
-    # Reset the ready lists: 
-    rlist, wlist = ([], []) 
-    if len(response) == 0: 
-        # If the response buffer is empty, 
-        # add socket to read list: 
+    # Reset the ready lists:
+    rlist, wlist = ([], [])
+    if len(response) == 0:
+        # If the response buffer is empty, add socket to read list:
         rlist = [sd]
         wlist = []
-    else: 
-        # Otherwise, add the socket to the 
-        # write list: 
+    else:
+        # Otherwise, add the socket to the write list:
         wlist = [sd]
         rlist = []
-        
-    # select (r,w,e) returns a tupple of the sockets that are actually readable, writeable or in error
+    
+    # select (r,w,e) returns a tuple of the sockets that are actually readable, writeable or in error
     rlist, wlist, xlist = select(rlist,wlist,[])
     if sd in rlist:
         try:
-            # Receive from the socket: 
-            request, src_addr = sd.recvfrom(72)
-            print 'Source: ' + src_addr[0] +' Requests: ' + request
-            if request == 'help' or request == '?': # send the help file
-                response = helpFile
+        # Receive from the socket:
+            data, src_addr = sd.recvfrom(1)
+            if data == TERMINATOR:
+                print 'Source: ' + src_addr[0] + ' / endpoint: ' + str(src_addr[1]) + ' / profile ID: ' + str(src_addr[2]) + ' / cluster ID: ' + str(src_addr[3]) + ' / Requests: ' + request
+            
+                if request == 'help': # send the help file
+                    response = helpFile
+                    request = ""
+                else:
+                    f = urllib.urlopen(request) # get the URL
+                    response = f.read() # parse the incoming data
+                    print '\tgot '+ str(len(response)) + ' bytes'
+                    request = ""
+            elif data == QUIT_CODE:
+                print "QUITTING"
+                break;
             else:
-                f = urllib.urlopen(request) # get the URL
-                response = f.read() # parse the incoming data
-                print '\tgot '+ str(len(response)) + ' bytes'
+                request += data
+	
         except Exception, e:
             response = '** request failed: ' + request + '\n\r\t' + str(e) + '\n\r'
             print '* request failed *'
             print e
+    
     if sd in wlist:
         try:
             # Send to the socket:
@@ -113,11 +137,12 @@ while (time.clock()-startTime < stopTime or stopTime==0):
             count = sd.sendto(part, 0, src_addr)
             # Slice off count bytes from the buffer
             response = response[count:]
-            time.sleep(0.00) # 0.01 WORKS FINE FOR 57600 BAUD but 0.5 is too slow for 9600 baud
+            time.sleep(SLEEP_DUR) # 0.01 WORKS FINE FOR 57600 BAUD but 0.5 is too slow for 9600 baud
         except Exception, e: #general exception handler
             print '* response failed *'
             print type(e)
             print e
             response = response[1:] # if a send fails, pare off the response and try again
+
 print 'stop time reached'
 print 'exiting normally'
