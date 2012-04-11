34c6858d9199b7998cb2346d30b8f1669fb3101c
Jordan Husney <jordan.husney@gmail.com>
2012-04-11 18:44:39 -0500

    added debug messages to console data pathway
    changed OSX build to be "semistandalone" testing with Rob, requires
    existing installation of Python 2.7 which should be ok on this environment

--- src/gui/handlers/xig_console.py
+++ src/gui/handlers/xig_console.py
@@ -3,6 +3,7 @@ import json
 import socket
 import select
 import threading
+import logging
 
 class XigConsoleHandler(threading.Thread):
     def __init__(self, port = None):
@@ -12,11 +13,13 @@ class XigConsoleHandler(threading.Thread):
         self.udp_sd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.received_data = ""
         self.lock = threading.RLock() #used for self.received_data
+        self.logger = logging.getLogger('xig.gui')
     
     def run(self):
         while 1:
             while self.port and select.select([self.udp_sd], [], [], 5)[0]:
                 buf = self.udp_sd.recv(4096)
+                self.logger.debug("CONSOLE: got %d bytes" % len(buf))
                 self.lock.acquire()
                 self.received_data += buf
                 self.lock.release()
