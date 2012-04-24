191de05a568e676a91888c8b6906a30d846527d4
Michael Sutherland <Michael.Sutherland@digi.com>
2012-04-23 17:37:13 -0700

    xig: fixed setup file for Windows build.

--- build/windows/setup.py
+++ build/windows/setup.py
@@ -1,25 +1,24 @@
 from distutils.core import setup
 import py2exe
 
-import os
-
-import xbee
-cp4pc_dir = os.path.dirname(xbee.__file__)
-
-import xig
-xig_src_dir = os.path.dirname(xig.__file__)
+import sys, os
+relative_dirs = ['../../src',
+                 '../../src/gui',
+                 '../../src/library/ext/cp4pc']
+dirs = [os.path.abspath(dir) for dir in relative_dirs]
+main_file = os.path.join(dirs[1], 'xig_app.py')
 
 python_modules = []
-dirs = [xig_src_dir, os.path.join(xig_src_dir, "gui"), cp4pc_dir]
 for dir in dirs:
+    sys.path.append(dir)
     for filename in os.listdir(dir):
         try:
             if filename[-3:] == ".py":
                 python_modules.append(filename[:-3])
         except:
             pass
-    
-setup(console=[os.path.join(xig_src_dir, "gui", 'xig_app.py')], 
+
+setup(console=[main_file], 
       options={
                "py2exe": {
                          "includes": python_modules,
