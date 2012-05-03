14e40dd188943f74b915a64932ec16c471e80774
Jordan Husney <jordan.husney@gmail.com>
2012-05-03 17:45:48 -0500

    bumping version number to 1.5.0b7

--- src/xig.py
+++ src/xig.py
@@ -12,7 +12,7 @@ See http://code.google.com/p/xig/ or README.txt for more information.
 ## Global String Constants
 NAME = "XBee Internet Gateway (XIG)"
 SHORTNAME = "xig"
-VERSION = "1.5.0b6"
+VERSION = "1.5.0b7"
 
 import sys
 
